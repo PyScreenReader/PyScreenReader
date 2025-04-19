ed62a37c996947c62979782a09bef6fb91263857
Minghao Li <lmh1230612306@163.com>
2025-04-19 19:43:32 -0400

    add -vv to pytest wrapper for verbose outputs

--- tests/py/tools/pytest_wrapper.py
+++ tests/py/tools/pytest_wrapper.py
@@ -1,5 +1,7 @@
 import sys
 import pytest
 
+PY_TEST_DEFAULT_ARGS = ['-vv']
+
 if __name__ == "__main__":
-    sys.exit(pytest.main(sys.argv[1:]))
+    sys.exit(pytest.main(sys.argv[1:] + PY_TEST_DEFAULT_ARGS))
