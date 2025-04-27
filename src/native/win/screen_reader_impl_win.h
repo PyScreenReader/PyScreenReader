52b2192714e0e90ed04d59ce35dedb8cf89adb28
Andrew Mao <72371419+Andrewyx@users.noreply.github.com>
2025-04-27 14:29:45 -0700

    cleanup

--- src/native/win/screen_reader_impl_win.h
+++ src/native/win/screen_reader_impl_win.h
@@ -15,9 +15,6 @@ namespace screen_reader
         [[nodiscard]] std::shared_ptr<VirtualRootWidget> getVirtualWidgetTreeByTitle(const std::string &title) const;
         [[nodiscard]] std::shared_ptr<VirtualRootWidget> getVirtualWidgetTreeByClassName(const std::string &className) const;
     private:
-        // CAUTION: Do not pass in the root (desktop) element. Traversing the entire subtree
-        // of the desktop could take a very long time and even lead to a stack overflow.
-        void ListDescendants(IUIAutomationElement* pParent, int indent, int depth) const;
         IUIAutomation* automation_;
         IUIAutomationElement* root_element_;
     };
