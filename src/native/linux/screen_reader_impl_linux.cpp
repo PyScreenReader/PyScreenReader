577dcb848639093271508c1d0fe431783fdc3c10
clay-arras <astrollin.neil@gmail.com>
2025-05-09 22:45:25 -0400

    manual menu mapping for vwidget generator

--- src/native/linux/screen_reader_impl_linux.cpp
+++ src/native/linux/screen_reader_impl_linux.cpp
@@ -1,20 +1,24 @@
 #include "src/native/linux/screen_reader_impl_linux.h"
+#include <memory>
 #include <stdexcept>
 
-namespace screen_reader
-{
-    std::shared_ptr<VirtualRootWidget> ScreenReaderImpl::getVirtualWidgetTreeByPID(const std::string &pid) const {
-        throw std::logic_error("Not yet implemented");
-        return nullptr;
-    }
-
-    std::shared_ptr<VirtualRootWidget> ScreenReaderImpl::getVirtualWidgetTreeByTitle(const std::string &title) const {
-        throw std::logic_error("Not yet implemented");
-        return nullptr;
-    }
-
-    std::shared_ptr<VirtualRootWidget> ScreenReaderImpl::getVirtualWidgetTreeByClassName(const std::string &className) const {
-        throw std::logic_error("Not yet implemented");
-        return nullptr;
-    }
+namespace screen_reader {
+std::shared_ptr<VirtualRootWidget>
+ScreenReaderImpl::getVirtualWidgetTreeByPID(const std::string &pid) const {
+  throw std::logic_error("Not yet implemented");
+  return nullptr;
 }
+
+std::shared_ptr<VirtualRootWidget>
+ScreenReaderImpl::getVirtualWidgetTreeByTitle(const std::string &title) const {
+  throw std::logic_error("Not yet implemented");
+  return nullptr;
+}
+
+std::shared_ptr<VirtualRootWidget>
+ScreenReaderImpl::getVirtualWidgetTreeByClassName(
+    const std::string &className) const {
+  throw std::logic_error("Not yet implemented");
+  return nullptr;
+}
+} // namespace screen_reader
