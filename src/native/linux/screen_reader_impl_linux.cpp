74b8ed3669374cc785ad2ecf132c6149809bcd40
clay-arras <astrollin.neil@gmail.com>
2025-05-24 22:12:00 -0400

    finished linux implementation

--- src/native/linux/screen_reader_impl_linux.cpp
+++ src/native/linux/screen_reader_impl_linux.cpp
@@ -1,11 +1,45 @@
-#include "src/native/linux/screen_reader_impl_linux.h"
-#include <memory>
+#include "screen_reader_impl_linux.h"
+#include "vwidget_generator.h"
+#include <cassert>
 #include <stdexcept>
+#include <string>
 
 namespace screen_reader {
 std::shared_ptr<VirtualRootWidget>
 ScreenReaderImpl::getVirtualWidgetTreeByPID(const std::string &pid) const {
-  throw std::logic_error("Not yet implemented");
+  atspi_init();
+  AtspiAccessible *desktop = atspi_get_desktop(0);
+  assert(desktop != nullptr && "Desktop does not exist");
+  AtspiAccessible *target_node = findAtspiAccessibleByPID(desktop, pid);
+
+  if (target_node == nullptr) {
+    throw std::runtime_error("Cannot find PID");
+  }
+
+  std::shared_ptr<VirtualRootWidget> widget_tree =
+      generator::GenerateVWidgetTree(target_node);
+
+  return widget_tree;
+}
+
+AtspiAccessible *
+ScreenReaderImpl::findAtspiAccessibleByPID(AtspiAccessible *node,
+                                           const std::string &pid) const {
+  if (node == nullptr) {
+    return nullptr;
+  }
+  if (std::to_string(atspi_accessible_get_process_id(node, nullptr)) == pid) {
+    return node;
+  }
+
+  size_t num_children = atspi_accessible_get_child_count(node, nullptr);
+  for (size_t i = 0; i < num_children; i++) {
+    AtspiAccessible *child = findAtspiAccessibleByPID(
+        atspi_accessible_get_child_at_index(node, i, nullptr), pid);
+    if (child != nullptr) {
+      return child;
+    }
+  }
   return nullptr;
 }
 
--- src/native/linux/screen_reader_impl_linux.h
+++ src/native/linux/screen_reader_impl_linux.h
@@ -1,18 +1,24 @@
 #pragma once
 
 #include "include/vwidget/widgets/virtual_root_widget.h"
+#include <atspi/atspi.h>
 #include <memory>
 
-namespace screen_reader 
-{
-    class ScreenReaderImpl 
-    {
-    public:
-        ScreenReaderImpl() = default;
-        ~ScreenReaderImpl() = default;
+namespace screen_reader {
+class ScreenReaderImpl {
+public:
+  ScreenReaderImpl() = default;
+  ~ScreenReaderImpl() = default;
 
-        [[nodiscard]] std::shared_ptr<VirtualRootWidget> getVirtualWidgetTreeByPID(const std::string &pid) const;
-        [[nodiscard]] std::shared_ptr<VirtualRootWidget> getVirtualWidgetTreeByTitle(const std::string &title) const;
-        [[nodiscard]] std::shared_ptr<VirtualRootWidget> getVirtualWidgetTreeByClassName(const std::string &className) const;
-    };
-}
+  [[nodiscard]] std::shared_ptr<VirtualRootWidget>
+  getVirtualWidgetTreeByPID(const std::string &pid) const;
+  [[nodiscard]] std::shared_ptr<VirtualRootWidget>
+  getVirtualWidgetTreeByTitle(const std::string &title) const;
+  [[nodiscard]] std::shared_ptr<VirtualRootWidget>
+  getVirtualWidgetTreeByClassName(const std::string &className) const;
+
+private:
+  AtspiAccessible *findAtspiAccessibleByPID(AtspiAccessible *node,
+                                            const std::string &pid) const;
+};
+} // namespace screen_reader
