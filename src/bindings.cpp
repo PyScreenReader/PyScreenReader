6921cc768ae885108bc51e32e76a3e8f5b8282ee
Minghao Li <lmh1230612306@163.com>
2025-06-08 22:57:55 -0700

    bind all setters

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -26,80 +26,115 @@ namespace py = pybind11;
  * @param module python module object
  */
 void BindVirtualWidgets(py::module_& module) {
+  // VirtualWidget
   py::class_<VirtualWidget, std::shared_ptr<VirtualWidget>>(module, "VirtualWidget")
       .def("get_title_text", &VirtualWidget::GetTitleText)
+      .def("set_title_text", &VirtualWidget::SetTitleText)
       .def("get_help_text", &VirtualWidget::GetHelpText)
+      .def("set_help_text", &VirtualWidget::SetHelpText)
       .def("get_native_name", &VirtualWidget::GetNativeName)
+      .def("set_native_name", &VirtualWidget::SetNativeName)
       .def("get_x", &VirtualWidget::GetX)
+      .def("set_x", &VirtualWidget::SetX)
       .def("get_y", &VirtualWidget::GetY)
+      .def("set_y", &VirtualWidget::SetY)
       .def("get_width", &VirtualWidget::GetWidth)
+      .def("set_width", &VirtualWidget::SetWidth)
       .def("get_height", &VirtualWidget::GetHeight)
+      .def("set_height", &VirtualWidget::SetHeight)
       .def("is_visible", &VirtualWidget::IsVisible)
+      .def("set_visible", &VirtualWidget::SetVisible)
       .def("is_focused", &VirtualWidget::IsFocused)
+      .def("set_focused", &VirtualWidget::SetFocused)
       .def("get_parent", &VirtualWidget::GetParent)
+      .def("set_parent", &VirtualWidget::SetParent)
       .def("get_child", &VirtualWidget::GetChild)
+      .def("add_child", &VirtualWidget::AddChild)
       .def("get_widget_name", &VirtualWidget::GetWidgetName);
 
+  // NumericValueWidget
   py::class_<NumericValueWidget, VirtualWidget,
                    std::shared_ptr<NumericValueWidget>>(module, "NumericValueWidget")
       .def("get_value", &NumericValueWidget::GetValue)
+      .def("set_value", &NumericValueWidget::SetValue)
       .def("get_min_value", &NumericValueWidget::GetMinValue)
-      .def("get_max_value", &NumericValueWidget::GetMaxValue);
+      .def("set_min_value", &NumericValueWidget::SetMinValue)
+      .def("get_max_value", &NumericValueWidget::GetMaxValue)
+      .def("set_max_value", &NumericValueWidget::SetMaxValue);
 
+  // VirtualButtonWidget
   py::class_<VirtualButtonWidget, VirtualWidget, std::shared_ptr<VirtualButtonWidget>>(
       module, "VirtualButtonWidget")
       .def(py::init());
 
+  // VirtualMenuItemWidget
   py::class_<VirtualMenuItemWidget, VirtualWidget, std::shared_ptr<VirtualMenuItemWidget>>(
       module, "VirtualMenuItemWidget");
 
+  // VirtualMenuWidget
   py::class_<VirtualMenuWidget, VirtualWidget, std::shared_ptr<VirtualMenuWidget>>(
       module, "VirtualMenuWidget")
       .def(py::init());
 
+  // VirtualProgressBarWidget
   py::class_<VirtualProgressBarWidget, NumericValueWidget,
              std::shared_ptr<VirtualProgressBarWidget>>(module, "VirtualProgressBarWidget")
       .def(py::init())
-      .def("get_orientation", &VirtualProgressBarWidget::GetOrientation);
+      .def("get_orientation", &VirtualProgressBarWidget::GetOrientation)
+      .def("set_orientation", &VirtualProgressBarWidget::SetOrientation);
 
+  // VirtualScrollBarWidget
   py::class_<VirtualScrollbarWidget, NumericValueWidget, std::shared_ptr<VirtualScrollbarWidget>>(
       module, "VirtualScrollbarWidget")
       .def(py::init())
-      .def("get_orientation", &VirtualScrollbarWidget::GetOrientation);
+      .def("get_orientation", &VirtualScrollbarWidget::GetOrientation)
+      .def("set_orientation", &VirtualScrollbarWidget::SetOrientation);
 
+  // VirtualSliderWidget
   py::class_<VirtualSliderWidget, NumericValueWidget, std::shared_ptr<VirtualSliderWidget>>(
       module, "VirtualSliderWidget")
       .def(py::init())
-      .def("get_orientation", &VirtualSliderWidget::GetOrientation);
+      .def("get_orientation", &VirtualSliderWidget::GetOrientation)
+      .def("set_orientation", &VirtualSliderWidget::SetOrientation);
 
+  // VirtualSpinnerWidget
   py::class_<VirtualSpinnerWidget, NumericValueWidget, std::shared_ptr<VirtualSpinnerWidget>>(
       module, "VirtualSpinnerWidget")
       .def(py::init());
 
+  // VirtualTextInputWidget
   py::class_<VirtualTextInputWidget, VirtualWidget, std::shared_ptr<VirtualTextInputWidget>>(
       module, "VirtualTextInputWidget")
       .def(py::init())
       .def("get_selected_text", &VirtualTextInputWidget::GetSelectedText)
+      .def("set_selected_text", &VirtualTextInputWidget::SetSelectedText)
       .def("get_insertion_point", &VirtualTextInputWidget::GetInsertionPoint)
-      .def("is_text_field", &VirtualTextInputWidget::IsTextField);
+      .def("set_insertion_point", &VirtualTextInputWidget::SetInsertionPoint)
+      .def("is_text_field", &VirtualTextInputWidget::IsTextField)
+      .def("set_is_text_field", &VirtualTextInputWidget::SetIsTextField);
 
+  // VirtualTextWidget
   py::class_<VirtualTextWidget, VirtualWidget, std::shared_ptr<VirtualTextWidget>>(
       module, "VirtualTextWidget")
       .def(py::init())
-      .def("get_selected_text", &VirtualTextWidget::GetSelectedText);
+      .def("get_selected_text", &VirtualTextWidget::GetSelectedText)
+      .def("set_selected_text", &VirtualTextWidget::SetSelectedText);
 
+  // VirtualUnknownWidget
   py::class_<VirtualUnknownWidget,
              std::shared_ptr<VirtualUnknownWidget>>(module, "VirtualUnknownWidget")
       .def(py::init());
 
+  // VirtualWindowWidget
   py::class_<VirtualWindowWidget, VirtualWidget, std::shared_ptr<VirtualWindowWidget>>(
       module, "VirtualWindowWidget")
       .def(py::init())
-      .def("is_modal", &VirtualWindowWidget::IsModal);
+      .def("is_modal", &VirtualWindowWidget::IsModal)
+      .def("set_is_modal", &VirtualWindowWidget::SetIsModal);
 }
 
 /**
- * Bind all screen reader APIS
+ * Bind all screen reader APIs
  *
  * @param module python module object
  */
