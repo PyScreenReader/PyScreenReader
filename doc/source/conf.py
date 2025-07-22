301766f41f959e03ed9f467b67a1d0a2cbf9385e
Minghao Li <lmh1230612306@163.com>
2025-07-22 00:35:30 -0700

    format sphinx config script

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,29 +1,29 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PyScreenReader'
-copyright = '2025, The PyScreenReader Team'
-author = 'The PyScreenReader Team'
-release = '0.0.1'
+project = "PyScreenReader"
+copyright = "2025, The PyScreenReader Team"
+author = "The PyScreenReader Team"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 # Markdown Parser
-extensions = ['myst_parser']
+extensions = ["myst_parser"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
