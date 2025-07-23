447b67914acaf0489702439545aca871d12ce7f1
Minghao Li <lmh1230612306@163.com>
2025-07-22 23:55:10 -0700

    revert numeric_value_widget.cpp

---
+++ src/vwidget/numeric_value_widget.cpp
@@ -0,0 +1,4 @@
+#include "include/vwidget/numeric_value_widget.h"
+#include "include/vwidget/virtual_widget.h"
+
+NumericValueWidget::NumericValueWidget(const std::string& widget_name) : VirtualWidget(widget_name) {}
