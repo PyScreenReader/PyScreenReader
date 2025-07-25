107d7cd0e2f45508b38ee0cd19ac31bbc1e7c8de
Minghao Li <lmh1230612306@163.com>
2025-07-25 00:30:02 -0700

    fix python linter

--- tests/py/vwidget/widgets/test_virtual_scrollbar_widget.py
+++ tests/py/vwidget/widgets/test_virtual_scrollbar_widget.py
@@ -1,5 +1,10 @@
 import pytest
-from PyScreenReader.virtual_widgets import NumericValueWidget, Orientation, VirtualScrollbarWidget, VirtualWidget
+from PyScreenReader.virtual_widgets import (
+    NumericValueWidget,
+    Orientation,
+    VirtualScrollbarWidget,
+    VirtualWidget,
+)
 
 from tests.py.tools.utils import (
     assert_all_base_functions_exist,
