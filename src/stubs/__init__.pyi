9867cab2d241460815108a70eea7e5f4e7c1be6e
Minghao Li <lmh1230612306@163.com>
2025-08-20 17:17:26 +0800

    implement stub generation for generate_doc script

--- src/stubs/__init__.pyi
+++ src/stubs/__init__.pyi
@@ -2,4 +2,4 @@ from __future__ import annotations
 from . import screen_reader
 from . import virtual_widgets
 __all__ = ['screen_reader', 'virtual_widgets']
-__version__: str = 'dev'
+__version__: str = '0.0.1'
