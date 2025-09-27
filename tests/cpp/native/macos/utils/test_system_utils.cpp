7aee519d72b6d49876cbcfd47e002efd317bb01e
Minghao Li <lmh1230612306@163.com>
2025-09-26 21:41:38 -0700

    fix test suite

--- tests/cpp/native/macos/utils/test_system_utils.cpp
+++ tests/cpp/native/macos/utils/test_system_utils.cpp
@@ -5,20 +5,15 @@
 
 TEST(SystemUtilsTest, ValidPIDReturnsAXUIElementRef) {
   pid_t current_pid = getpid();
-  std::string pid_str = std::to_string(current_pid);
-  AXUIElementRef element = system_utils::GetAXUIElementRefByPID(pid_str);
+  AXUIElementRef element = system_utils::GetAXUIElementRefByPID(current_pid);
   ASSERT_NE(element, nullptr);
   CFRelease(element);
 }
 
-TEST(SystemUtilsTest, InvalidPIDThrows) {
-  std::string invalid_pid = "abc123";
-  EXPECT_THROW({ system_utils::GetAXUIElementRefByPID(invalid_pid); }, std::invalid_argument);
-}
 
 TEST(SystemUtilsTest, NonExistentPIDStillReturnsNonNullElement) {
   // AXUIElementCreateApplication still returns a valid ref even for dead PIDs
-  std::string fake_pid = "999999";
+  pid_t fake_pid = 999999;
   AXUIElementRef element = system_utils::GetAXUIElementRefByPID(fake_pid);
   ASSERT_NE(element, nullptr);
   CFRelease(element);
